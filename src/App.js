73aea478b9918417183aea09574817d06b75cb61
joshinrai <joshinrai@163.com>
2017-05-15 11:05:08 +0800

    个人主页
    个人主页

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contant from './contant' ;
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
         </div>
         <p className="App-intro">
           ©joshinrai
+          <contant />
         </p>
       </div>
     );
