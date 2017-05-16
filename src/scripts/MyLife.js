378488e77266be1a91e1427069e434cad55d59b3
joshinrai <joshinrai@163.com>
2017-05-16 14:56:23 +0800

    添加图标
    添加图标

--- src/scripts/MyLife.js
+++ src/scripts/MyLife.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import github from "../resource/github.jpg" ;
+import email from "../resource/email.png" ;
+import homepage from "../../public/joshinrai.jpg" ;
 
 class MyLife extends Component {
   render() {
@@ -10,8 +13,17 @@ class MyLife extends Component {
           <li>游戏
           </li>
           <li>与我联系:
-              <p>email:joshinrai@163.com</p>
-              <p>github:https://github.com/joshinrai</p>
+            <p>
+              <a href="mailto:joshinrai@163.com">
+                <img src={email} alt="email" className="icon-style"/>
+              </a>
+              <a href="http://47.93.0.22:3000" target="_blank">
+                <img src={homepage} alt="homepage" className="icon-style"/>
+              </a>
+              <a href="https://github.com/joshinrai" target="_blank">
+                <img src={github} alt="github" className="icon-style"/>
+              </a>
+            </p>
           </li>
         </ul>
       </div>
