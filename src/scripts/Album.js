0c84dac44667267d085c1a6a99466847b129a85f
joshinrai <joshinrai@163.com>
2017-05-16 13:23:10 +0800

    设置轮播
    设置轮播

--- src/scripts/Album.js
+++ src/scripts/Album.js
@@ -6,30 +6,40 @@ import img3 from '../resource/img3.jpg' ;
 import img4 from '../resource/img4.jpg' ;
 import img5 from '../resource/img5.jpg' ;
 
+var albumArr = [{ src : img1 , alt : "img1" },{ src : img2 , alt : "img2"},
+				{ src : img3 , alt : "img3" },{ src : img4 , alt : "img4"},
+				{ src : img5 , alt : "img5" }] ;
+
+class AlbumList extends Component {
+	functionTest(){
+		console.log("this is function test ...") ;
+	}
+	render(){
+		var self = this ;
+		var menus = this.props.items.map(function(item){
+			return <li key={item.alt} onClick={self.functionTest}>
+						<img className="img-style" src={item.src} alt={item.alt}/>
+					</li> ;
+		}) ;
+		return <ul className="cell-module">{menus}</ul> ;
+	}
+}
+
 class Album extends Component {
-  render() {
-    return (
-      <div>
-        <ul className="cell-module">
-          <li>
-            <img className="img-style" src={img1} alt="img1"/>
-          </li>
-          <li>
-            <img className="img-style" src={img2} alt="img2"/>
-          </li>
-          <li>
-            <img className="img-style" src={img3} alt="img3"/>
-          </li>
-          <li>
-            <img className="img-style" src={img4} alt="img4"/>
-          </li>
-          <li>
-            <img className="img-style" src={img5} alt="img5"/>
-          </li>
-        </ul>
-      </div>
-    );
-  }
+	/*intervalFunction(){
+		setInterval(function(){
+			albumArr.unshift(albumArr[albumArr.length-1]) ;
+			albumArr.pop() ;
+		} , 2000) ;
+	}*/
+  	render() {
+  		//this.intervalFunction() ;
+	    return (
+	      <div>
+	      	<AlbumList items={albumArr}/>
+	      </div>
+	    );
+  	}
 }
 
 export default Album;
